c869a5ee9f84fa81b0201bc20ecf0f8efdefaee8
SpringSunYY <3217778348@qq.com>
2024-10-16 13:05:49 +0800

    完成优化生成页面 待完成：设计生成逻辑

--- RuoYi-Vue/ruoyi-ui/src/views/tool/gen/genValue.vue
+++ RuoYi-Vue/ruoyi-ui/src/views/tool/gen/genValue.vue
@@ -1,35 +1,101 @@
 <template>
   <el-card>
-    <el-tabs :tab-position="tabPosition" style="margin-bottom: 30px;height: 500px">
+    <el-tabs :tab-position="tabPosition" type="border-card" style="margin-bottom: 30px;min-height: 600px">
       <el-tab-pane
         v-for="(column,index) in columns"
         :key="column.columnId"
-        :label="column.columnName"
         :name="column.columnName"
       >
+        <span slot="label"><svg-icon v-if="column.required" icon-class="star" style="color: red"/> {{
+            column.columnName
+          }}</span>
         <el-form :ref="'form' + index" :model="tableValues[index]" label-width="80px" size="mini"
                  style="width: 500px; margin: 0 auto;"
         >
+          <el-form-item label="字段名称">
+            <span>{{ tableValues[index].columnName }}</span>
+          </el-form-item>
           <el-form-item label="字段描述">
-            {{ tableValues[index].columnComment }}
-          </el-form-item> <el-form-item label="物理类型">
-            {{ tableValues[index].columnType }}
+            <span>{{ tableValues[index].columnComment }}</span>
+          </el-form-item>
+          <el-form-item label="物理类型">
+            <span> {{ tableValues[index].columnType }}</span>
           </el-form-item>
           <el-form-item label="Java类型">
-            {{ tableValues[index].javaType }}
+            <span>{{ tableValues[index].javaType }}</span>
           </el-form-item>
           <el-form-item label="Java属性">
-            {{ tableValues[index].javaField }}
+            <span>{{ tableValues[index].javaField }}</span>
           </el-form-item>
-          <el-form-item label="生成数值">
-            <el-input v-model="tableValues[index].genValue" placeholder="请输入生成数值" />
+          <el-form-item label="是否唯一">
+            <el-radio-group v-model="tableValues[index].isSole">
+              <el-radio :label="0">否</el-radio>
+              <el-radio :label="1">是</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item v-if="column.htmlType==='textarea'"
+                        label="生成数值"
+                        :rules="[
+                      { required: column.required, message: `${column.columnComment} 不能为空`, trigger: 'blur' }
+                    ]"
+          >
+            <el-input v-model="tableValues[index].value" type="textarea" placeholder="请输入生成数值"/>
+          </el-form-item>
+          <el-form-item v-if="column.htmlType==='datetime'"
+                        label="生成数值"
+                        :rules="[
+                      { required: column.required, message: `${column.columnComment} 不能为空`, trigger: 'blur' }
+                    ]"
+          >
+            <el-date-picker clearable
+                            v-model="tableValues[index].value"
+                            type="date"
+                            value-format="yyyy-MM-dd"
+                            placeholder="请选择日期"
+            >
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item v-if="column.htmlType==='imageUpload'"
+                        label="生成数值"
+                        :rules="[
+                      { required: column.required, message: `${column.columnComment} 不能为空`, trigger: 'blur' }
+                    ]"
+          >
+            <image-upload v-model="tableValues[index].value"/>
+          </el-form-item>
+          <el-form-item v-if="column.htmlType==='fileUpload'"
+                        label="生成数值"
+                        :rules="[
+                      { required: column.required, message: `${column.columnComment} 不能为空`, trigger: 'blur' }
+                    ]"
+          >
+            <file-upload v-model="tableValues[index].value"/>
+          </el-form-item>
+          <el-form-item v-if="column.htmlType==='editor'"
+                        label="生成数值"
+                        :rules="[
+                      { required: column.required, message: `${column.columnComment} 不能为空`, trigger: 'blur' }
+                    ]"
+          >
+            <editor v-model="tableValues[index].value"/>
+          </el-form-item>
+          <el-form-item
+            label="生成数值"
+            :rules="[
+                      { required: column.required, message: `${column.columnComment} 不能为空`, trigger: 'blur' }
+                    ]"
+          >
+            <el-input v-model="tableValues[index].value" placeholder="请输入生成数值"/>
           </el-form-item>
         </el-form>
         {{ tableValues[index] }}
       </el-tab-pane>
     </el-tabs>
-    <el-form label-width="100px">
-      <el-form-item style="text-align: center;margin-left:-100px;margin-top:10px;">
+    <el-form label-width="1000px" :inline="true" model="formInline" class="demo-form-inline">
+      <el-form-item label="生成总数">
+        <el-input v-model="genInfo.genNumbers" placeholder="请输入生成总数"></el-input>
+      </el-form-item>
+      <el-form-item >
         <el-button type="primary" @click="submitForm()">提交</el-button>
         <el-button @click="close()">返回</el-button>
       </el-form-item>
@@ -53,6 +119,8 @@ export default {
   },
   data() {
     return {
+      //生成信息
+      genInfo: {},
       tabPosition: 'top',
       tableValues: [],
       // 选中选项卡的 name
@@ -77,13 +145,15 @@ export default {
       // 获取表详细信息
       getGenTable(tableId).then(res => {
         this.columns = res.data.rows
-        console.log(this.columns)
         // 初始化 tableValues，使其长度与 columns 一致
         this.tableValues = this.columns.map((item) => ({
+          columnName: item.columnName,
           columnComment: item.columnComment,
           columnType: item.columnType,
           javaType: item.javaType,
           javaField: item.javaField,
+          required: item.required,
+          isSole: 0
         }))
         this.info = res.data.info
         this.tables = res.data.tables
@@ -101,15 +171,27 @@ export default {
   methods: {
     /** 提交按钮 */
     submitForm() {
+      //校验是否有必须需要值但是为空的
+      for (const index in this.columns) {
+        const column = this.columns[index]
+        const value = this.tableValues[index]?.value // 获取对应的值
+        if (column.required && (!value || value.trim() === '')) {
+          // 如果 required 为 true 且值不存在或为空
+          this.$message.error(`${column.columnComment}不能为空`)
+          return // 直接返回
+        }
+      }
       console.log(this.tableValues)
-    },
+    }
+    ,
     getFormPromise(form) {
       return new Promise(resolve => {
         form.validate(res => {
           resolve(res)
         })
       })
-    },
+    }
+    ,
     /** 关闭按钮 */
     close() {
       const obj = { path: '/tool/gen', query: { t: Date.now(), pageNum: this.$route.query.pageNum } }
